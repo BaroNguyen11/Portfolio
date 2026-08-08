0884042d1cb2cb527659919d47899509926f3500
Nguyễn Văn Bảo <bao927471@gmail.com>
2026-08-08 19:20:34 +0700

    change filename

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
             the links below.
           </p>
 
-          {/* Email */}
+          {/* Email */} 
 
           <a
             href="https://mail.google.com/mail/?view=cm&fs=1&to=bao927471@gmail.com"
